f001ed2f88271d5e802430c550962eb8dd35baf7
DevonMcfarlen <mcfarlendevon@gmail.com>
2023-11-29 19:58:03 -0600

    created and filled out routes

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 const {connect, connection} = require('mongoose');
 
 const connectionString =
-  process.env.MONGODB_URI ||'mongodb://127.0.0.1:27017/studentsDB';
+  process.env.MONGODB_URI || 'mongodb+srv://root:P5nMvrDA6J5XT7ox@uta-bootcamp-01.dwuw1sk.mongodb.net/';
 
 connect(connectionString);
 
